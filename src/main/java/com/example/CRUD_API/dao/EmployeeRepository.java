9cae37f5c0ea040601cd454482e5e5d0027e4375
Nikhil Khilwani <khilwaninikhil22@gmail.com>
2025-06-22 22:01:59 +0530

    Data REST - Configuration, Pagination and Sorting

--- src/main/java/com/example/CRUD_API/dao/EmployeeRepository.java
+++ src/main/java/com/example/CRUD_API/dao/EmployeeRepository.java
@@ -2,5 +2,7 @@ package com.example.CRUD_API.dao;
 
 import com.example.CRUD_API.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+//@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> { }
